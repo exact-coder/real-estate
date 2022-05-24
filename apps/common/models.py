180ab001ba5169843c3057d920267dc32a9bd6f4
Programming Bees <hasanakash799@gmail.com>
2022-05-24 18:12:35 +0600

    feat: add profile model and signals

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+import uuid
 
 # Create your models here.
+
+class TimeStampUUIDModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4,editable=False,unique=True)
+    created_at = models.DateTimeField( auto_now_add=True)
+    updated_at = models.DateTimeField( auto_now=True)
+
+    class Meta:
+        abstract =True
+
